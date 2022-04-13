b0a28a2449f5fa8d870cfbecd0b0c5848cad6205
Kondziu <kondziu@MacBook-Air-Kondziu.local>
2022-04-13 14:39:45 +0200

    s

--- JavaScript/photoEdit.js
+++ JavaScript/photoEdit.js
@@ -1,11 +1,17 @@
+
+
 const uploadBtn = document.getElementById("upload-btn");
 const inputUploadBtn = document.getElementById("input-upload-btn");
 let chosenImage = document.getElementById("chosen-image");
+const downloadImg = document.getElementById("download-img")
 
 let contrast = document.getElementById("contrast");
 let hueRotate = document.getElementById("hue-rotate");
 let sepia = document.getElementById("sepia");
 let blur = document.getElementById("blur");
+let brightness = document.getElementById("brightness")
+let saturate = document.getElementById("saturate")
+
 let mirrorBtn = document.getElementById("mirror-btn");
 
 uploadBtn.onclick = () => {
@@ -23,13 +29,23 @@ inputUploadBtn.onchange = () => {
     };
 };
 function addFilter() {
-    chosenImage.style.filter = `contrast(${contrast.value}%) hue-rotate(${hueRotate.value}deg) sepia(${sepia.value}%) blur(${blur.value}px)`;
-    console.log(chosenImage.style.filter)
+    chosenImage.style.filter = `contrast(${contrast.value}%) hue-rotate(${hueRotate.value}deg) sepia(${sepia.value}%) blur(${blur.value}px) brightness(${brightness.value}%) saturate(${saturate.value}%)`;
 }
 
 let sliders = document.querySelectorAll(".filter input[type='range']");
 sliders.forEach( slider => {
     slider.addEventListener("input", addFilter);
-    console.log(slider)
 });
 
+mirrorBtn.onclick = () =>{
+    chosenImage.classList.toggle('mirror')
+}
+
+downloadImg.addEventListener('click', ()=>{
+    let imgPath = chosenImage.getAttribute('src').style.filter
+    let fileName = getFileName(imgPath)
+    saveAs(imgPath,fileName)
+})
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/')+1)
+}
