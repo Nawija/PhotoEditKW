1788db042fb6000006c7e7a71ba96805cfa307d0
Kondziu <kondziu@MacBook-Air-Kondziu.local>
2022-04-13 17:50:22 +0200

    s

---
+++ JavaScript/gameXo.js
@@ -0,0 +1,7 @@
+const turn = document.getElementById('turn')
+let xo = document.getElementById('xo')
+const square = document.querySelectorAll('.square')
+
+square.forEach(squar =>{
+    console.log(squar + 'xxx')
+})
